1b33d7cb6f80ccf989bdd72ff6ac38aef3bc8d76
ethxndxvid <ethanbrian.david@gmail.com>
2023-03-12 15:39:05 +0200

    Created basic structure of calc

---
+++ calculatorV1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    char operator;
+    double operand1, operand2;
+
+    printf("Enter two operands: \n");
+    scanf("%lf %lf", &operand1, &operand2);
+    printf("Enter an operator: \n");
+    scanf("%c", &operator);
+
+
+
+    return 0;
+}
