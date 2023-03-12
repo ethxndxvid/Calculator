e506df2960a874800fa9a1e682287ad62f07ca1d
ethxndxvid <ethanbrian.david@gmail.com>
2023-03-12 15:56:28 +0200

    Added switch statement to compute expression

--- calculatorV1.c
+++ calculatorV1.c
@@ -7,10 +7,28 @@ int main(){
 
     printf("Enter two operands: \n");
     scanf("%lf %lf", &operand1, &operand2);
-    printf("Enter an operator: \n");
-    scanf("%c", &operator);
+    printf("Enter an operator(+, -, *, /): \n");
+    scanf(" %c", &operator);
 
+    switch (operator)
+    {
+    case '+':
+        printf("%lf + %lf = %lf", operand1, operand2, operand1 + operand2);
+        break;
+    
+    case '-':
+        printf("%lf - %lf = %lf", operand1, operand2, operand1 - operand2);
+        break;
+    
+    case '*':
+        printf("%lf * %lf = %lf", operand1, operand2, operand1 * operand2);
+        break;
 
-
+    case '/':
+        printf("%lf / %lf = %lf", operand1, operand2, operand1 / operand2);
+    default:
+        printf("Error! Incorrect operator!");
+        break;
+    }
     return 0;
 }
